b2f3dc44cd5a6c268ada53ee6fc52543115ecd2e
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-02-16 22:17:24 +0100

    Add additional fields to WorkflowInstanceIndex mapping
    Expanded the WorkflowInstanceIndex mapping to include new fields such as Status, CreatedAt, FinishedAt, and others. These additions enhance the indexing capabilities by capturing more detailed workflow information, including correlations and incident status.

--- src/OrchardCore.ElsaWorkflows/Indexes/WorkflowInstanceIndexProvider.cs
+++ src/OrchardCore.ElsaWorkflows/Indexes/WorkflowInstanceIndexProvider.cs
@@ -9,7 +9,7 @@ public class WorkflowInstanceIndexProvider : IndexProvider<WorkflowInstance>
     {
         CollectionName = ElsaCollections.WorkflowInstances;
     }
-    
+
     public override void Describe(DescribeContext<WorkflowInstance> context)
     {
         context.For<WorkflowInstanceIndex>().Map(instance => new()
@@ -19,6 +19,16 @@ public class WorkflowInstanceIndexProvider : IndexProvider<WorkflowInstance>
             DefinitionVersionId = instance.DefinitionVersionId,
             Version = instance.Version,
             Name = instance.Name,
+            Status = instance.Status,
+            CreatedAt = instance.CreatedAt,
+            FinishedAt = instance.FinishedAt,
+            CorrelationId = instance.CorrelationId,
+            IncidentCount = instance.IncidentCount,
+            IsSystem = instance.IsSystem,
+            SubStatus = instance.SubStatus,
+            UpdatedAt = instance.UpdatedAt,
+            ParentInstanceId = instance.ParentWorkflowInstanceId,
+            HasIncidents = instance.IncidentCount > 0
         });
     }
 }
