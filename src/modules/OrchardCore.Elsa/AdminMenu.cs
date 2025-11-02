4df34825604144f31bf6c160eb165691aa4b3413
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-11-02 10:47:20 +0100

    Remove legacy navigation format from admin menu and simplify configuration
    Eliminated outdated `UseLegacyFormat` checks and streamlined the admin menu setup for Elsa workflows. Updated navigation views to use the modern format exclusively, removing redundant `icon-none` elements.

--- src/modules/OrchardCore.Elsa/AdminMenu.cs
+++ src/modules/OrchardCore.Elsa/AdminMenu.cs
@@ -12,44 +12,21 @@ public class AdminMenu(IStringLocalizer<AdminMenu> localizer) : INavigationProvi
         if (!string.Equals(name, "admin", StringComparison.OrdinalIgnoreCase))
             return ValueTask.CompletedTask;
 
-        if (NavigationHelper.UseLegacyFormat())
-        {
-            builder
-                .Add(T["Elsa"], NavigationConstants.AdminMenuWorkflowsPosition, workflow =>
-                {
-                    workflow
-                        .AddClass("elsa")
-                        .Id("elsa")
-                        .Add(T["Workflow Definitions"], workflowDefinitions => workflowDefinitions
-                            .Action("List", "WorkflowDefinitions", new { area = Constants.Area })
-                            .Permission(Permissions.ManageWorkflows)
-                            .LocalNav())
-                        .Add(T["Workflow Instances"], workflowInstances => workflowInstances
-                            .Action("List", "WorkflowInstances", new { area = Constants.Area })
-                            .Permission(Permissions.ManageWorkflows)
-                            .LocalNav());
-                });
-
-            return ValueTask.CompletedTask;
-        }
-
         builder
-            .Add(T["Tools"], tools => tools
-                .Add(T["Elsa"], T["WorkflowsB"].PrefixPosition(), workflow =>
-                {
-                    workflow
-                        .AddClass("elsa")
-                        .Id("elsa")
-                        .Add(T["Definitions"], workflowDefinitions => workflowDefinitions
-                            .Action("List", "WorkflowDefinitions", new { area = Constants.Area })
-                            .Permission(Permissions.ManageWorkflows)
-                            .LocalNav())
-                        .Add(T["Instances"], workflowInstances => workflowInstances
-                            .Action("List", "WorkflowInstances", new { area = Constants.Area })
-                            .Permission(Permissions.ManageWorkflows)
-                            .LocalNav());
-                })
-            );
+        .Add(T["Elsa Workflows"], NavigationConstants.AdminMenuWorkflowsPosition, workflow =>
+        {
+            workflow
+                .AddClass("elsa")
+                .Id("elsa")
+                .Add(T["Definitions"], workflowDefinitions => workflowDefinitions
+                    .Action("List", "WorkflowDefinitions", new { area = Constants.Area })
+                    .Permission(Permissions.ManageWorkflows)
+                    .LocalNav())
+                .Add(T["Instances"], workflowInstances => workflowInstances
+                    .Action("List", "WorkflowInstances", new { area = Constants.Area })
+                    .Permission(Permissions.ManageWorkflows)
+                    .LocalNav());
+        });
 
         return ValueTask.CompletedTask;
     }
